42f93dbcde98b434d34e1975c31a85ba5089b275
Michael Charland <michael.charland@textnow.com>
2017-12-14 08:15:40 -0500

    156 passing

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b && b == c
+    return :equilateral
+  end
+
+    if a ==b || a == c || b == c
+      return :isosceles
+    end
+
+      :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
